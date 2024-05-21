646d3d9c4a53d758e1b662de6c2d7256d1a9e33d
YullKim <yull618@naver.com>
2024-05-21 10:02:53 +0900

    Add src files

---
+++ 0521_DeviceDriver/DeviceDriver.h
@@ -0,0 +1,14 @@
+﻿#pragma once
+#pragma once
+#include "FlashMemoryDevice.h"
+
+class DeviceDriver
+{
+public:
+    DeviceDriver(FlashMemoryDevice* hardware);
+    int read(long address);
+    void write(long address, int data);
+
+protected:
+    FlashMemoryDevice* m_hardware;
+};
